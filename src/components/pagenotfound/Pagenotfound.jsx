88c55636c2e9362316e973636efeebb3e02252f1
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-10-03 22:19:08 -0500

    feat: :sparkles: Component Page Not Found
    Page not found component is created
    
    by: Jhon Hernandez

--- src/components/pagenotfound/Pagenotfound.jsx
+++ src/components/pagenotfound/Pagenotfound.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import './Pagenotfound.css'
 
 export default function Pagenotfound() {
   return (
-    <div>Pagenotfound</div>
+    <div className='positionAbsolutePage'>
+      <div className='contenedorColor'>
+
+        <div>
+          <div class="starsec"></div>
+          <div class="starthird"></div>
+          <div class="starfourth"></div>
+          <div class="starfifth"></div>
+        </div>
+
+        <div class="lamp__wrap">
+          <div class="lamp">
+            <div class="cable"></div>
+            <div class="coverLampara"></div>
+            <div class="in-cover">
+              <div class="bulb"></div>
+            </div>
+            <div class="light"></div>
+          </div>
+        </div>
+        <section class="error">
+          <div class="error__content">
+            <div class="error__message message">
+              <h1 class="message__title">Page Not Found</h1>
+            </div>
+          </div>
+        </section>
+
+      </div>
+    </div>
   )
 }
